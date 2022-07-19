18ab8e7cc08844fce60468c27777855392ace40d
Pablo Caram <pablocaram@gmail.com>
2022-07-19 18:01:31 -0300

    2do_entregable-PabloCaram

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <button>Menú</button>
+        <button>Productos</button>
+        <button>Servicios</button>
+        <button>Nosotros</button>
+    </div>
+  )
+}
+
+export default Navbar
